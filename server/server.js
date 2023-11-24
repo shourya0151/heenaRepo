4658d7fe612c9a00e53700e68c9d8e10e00eb1bf
Shourya Gupta <shouryaguptag11@gmail.com>
2023-11-25 00:18:34 +0530

    second commit

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const authRoutes = require('./routes/authRoutes');
+const taskRouter = require('./routes/taskRoutes');
+const morgan = require('morgan');
+
+
+
+const mongoose = require('mongoose');
+
+const mongoURI = 'mongodb+srv://test:12345@cluster0.you7nqg.mongodb.net/?retryWrites=true&w=majority';
+const options = {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+};
+
+mongoose
+	.connect(mongoURI, options)
+	.then(() => {
+		console.log('Connected to MongoDB');
+		// Start your application or perform additional operations
+	})
+	.catch((error) => {
+		console.error('Error connecting to MongoDB:', error);
+	});
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use('/auth', authRoutes);
+app.use('/task', taskRouter);
+
+// localhost:4000/auth/register
+
+const port = 4000;
+
+app.listen(port, () => {
+	console.log(`server is running on port`, port);
+});
